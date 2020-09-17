b797b68b1fe2c44313b7c224b308f8abae7d5565
irfanasandi <irfanasandi21@gmail.com>
2020-09-17 23:55:10 +0700

    add constant

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
 
-const kReleaseMode = false;
-
 const kPrimaryColor = Color(0xFFFF7643);
 const kPrimaryLightColor = Color(0xFFFFECDF);
 const kPrimaryGradientColor = LinearGradient(
@@ -13,3 +11,12 @@ const kSecondaryColor = Color(0xFF979797);
 const kTextColor = Color(0xFF757575);
 
 const kAnimationDuration = Duration(milliseconds: 200);
+
+// Form Error
+final RegExp emailValidatorRegExp =
+    RegExp(r"^[a-zA-Z0-9.]+@[a-zA-Z0-9]+\.[a-zA-Z]+");
+const String kEmailNullError = "Please Enter your email";
+const String kInvalidEmailError = "Please Enter Valid Email";
+const String kPassNullError = "Please Enter your password";
+const String kShortPassError = "Password is too short";
+const String kMatchPassError = "Passwords don't match";
